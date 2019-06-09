f6c4e2e5cfa04fafdc3faf15ddc3fba488a27915
Soundharyaam <soundharyabharatiya@gmail.com>
2019-06-09 15:34:34 +0530

    Local Storage for adding and removing the entries

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,40 +7,46 @@ import { Question } from "../models/Question";
 export class DataService {
   questions:Question[];
   constructor() { 
-    this.questions = [
-      {
-        text:"What is your name?",
-        answer: "My name is Soundharya",
-        hide:true
-      },
-      {
-        text:"What is your favorite color?",
-        answer: "My favorite color is pink",
-        hide:true
-      },
-      {
-        text:"What is your favorite language?",
-        answer: "My favorite language is Tamil",
-        hide:true
-      }
-    ];
+    
   }
 
+  //Get Question from LS
   getQuestions(){
+    if(localStorage.getItem('questions')===null){
+      this.questions=[];
+    }
+    else{
+      this.questions=JSON.parse(localStorage.getItem('questions'));
+    }
     return this.questions;
   }
 
+  //Add Question from LS
   addQuestion(question:Question){
     this.questions.unshift(question);
+
+    let questions;
+    if(localStorage.getItem('questions')===null){
+      questions=[];
+      questions.unshift(question);
+      localStorage.setItem('questions',JSON.stringify(questions));
+    }
+    else{
+      questions=JSON.parse(localStorage.getItem('questions'));
+      questions.unshift(question);
+      localStorage.setItem('questions',JSON.stringify(questions));    
+    }
+
   }
   
+  //Remove Question from LS
   removeQuestion(question: Question){
     for(let i=0;i<this.questions.length;i++){
       if(question == this.questions[i]){
         this.questions.splice(i,1);
+        localStorage.setItem('questions', JSON.stringify(this.questions));
       }
     }
-    // this.questions.removeQuestion(question);
   }
 
 }
